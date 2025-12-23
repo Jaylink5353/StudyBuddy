098bf400f6d7faefe9f4a8fd597ec04174c49ac9
Jaylink5353 <jaylink5353@gmail.com>
2025-12-23 16:36:57 -0500

    Started arduino code. Made buzzer, and currently working on button logic

--- Arduino/StudyBuddy.ino/StudyBuddy.ino.ino
+++ Arduino/StudyBuddy.ino/StudyBuddy.ino.ino
@@ -1,9 +1,51 @@
-void setup() {
-  // put your setup code here, to run once:
+#include <Adafruit_SSD1306.h>
+#include <splash.h>
+#define BUZZER_PIN 10
+#define BUTTON_PIN A0
 
+void setup() {
+  //pin setup
+  pinMode(BUZZER_PIN, OUTPUT);
+  pinMode(BUTTON_PIN, INPUT);
+  //serial init
+  Serial.begin(9600);
+  //beep to signify power on or code restart
+  digitalWrite(BUZZER_PIN, HIGH);
+  delay(150);
+  digitalWrite(BUZZER_PIN, LOW);
+  
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  //temp to init arduino code.
+  readButton();
+  delay(1000);
+}
+
+void soundAlarm(){
+  digitalWrite(BUZZER_PIN, HIGH);
+  delay(500);
+  digitalWrite(BUZZER_PIN, LOW);
+  delay(350);
+  digitalWrite(BUZZER_PIN, HIGH);
+  delay(150);
+  digitalWrite(BUZZER_PIN, LOW);
+  delay(150);
+  digitalWrite(BUZZER_PIN, HIGH);
+  delay(150);
+  digitalWrite(BUZZER_PIN, LOW);
+  delay(150);
+}
+void readButton(){
+  int readValue = analogRead(BUTTON_PIN);
+  Serial.print(readValue);
+  if (readValue >= 98 && readValue <= 170){
+    Serial.println("Closest to OLED");
+  }
+  if (readValue >= 514 && readValue <= 570){
+    Serial.println("Middle Button!");
+    soundAlarm();
+  }
+  if (readValue >= 490 && readValue <= 505){
+    Serial.println("Middle Button");
+  }
 }
